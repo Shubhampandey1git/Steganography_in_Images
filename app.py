c8f77b2173b73d8e0e1373d3426d7226e80bc44e
shubham <shubhamppandey1084@gmail.com>
2024-10-17 12:10:22 +0530

    Add environment variables

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def extract_form():
     return render_template('extract.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
